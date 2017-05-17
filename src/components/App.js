c24f0c04228f34542198fd2c40ed04e7ae8ec829
Peter <peter.aguilar2287@gmail.com>
2017-05-17 01:50:13 -0400

    initial commit - create app structure and piping

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component, PropTypes } from 'react'
+import Navigation from './common/Navigation'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+App.propTypes = {
+    children: PropTypes.object.isRequired
+}
+
+export default App
