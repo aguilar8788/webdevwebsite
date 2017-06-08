6f368d4d766e4e7c6b43abebce0dbe0cb0b8c251
Peter <peter.aguilar2287@gmail.com>
2017-06-08 13:49:21 -0400

    add gyphicon to social drop down and nav styling

--- src/components/common/navBar/component/NavBar.js
+++ src/components/common/navBar/component/NavBar.js
@@ -1,14 +1,42 @@
 import React, {Proptypes, Component} from 'react'
 import { Link, IndexLink } from 'react-router'
 import {connect} from 'react-redux'
+import Glyphicon from 'react-bootstrap/lib/Glyphicon'
+
 class NavBar extends Component {
 	constructor(props, context) {
 		super(props, context)	
+
+		this.showSocialDropDown = this.showSocialDropDown.bind(this)
+		this.renderDropDownGlyph = this.renderDropDownGlyph.bind(this)
 	}	
 
 	componentWillMount() {
 		this.setState({showSocial: false})
 	}
+
+	showSocialDropDown() {
+		let showSocialBool = this.state.showSocial
+		//this.state.showSocial ? this.setState({showSocial: false}) : this.setState({showSocial: true})
+		this.setState({showSocial: showSocialBool ? false : true})
+	}
+
+	renderDropDownGlyph() {
+		if(this.state.showSocial == true) {
+		return(
+			<span>
+       <Glyphicon glyph="glyphicon glyphicon-chevron-up"/>
+    </span>	
+		)	
+		} else {
+			return(
+			<span>
+       <Glyphicon glyph="glyphicon glyphicon-chevron-down"/>
+    </span>	
+		)
+		}
+	}
+	
 
 	render () {
 		return (
@@ -18,10 +46,10 @@ class NavBar extends Component {
 					<li><IndexLink to="/work" activeClassName="active">work</IndexLink></li>
 					<li><IndexLink to="/blog" activeClassName="active">blog</IndexLink></li>
 					<li><IndexLink to="/contact" activeClassName="active">contact</IndexLink></li>
-					<li className="active" onClick={() => this.state.showSocial ? this.setState({showSocial: false}) : this.setState({showSocial: true})}>social</li>
-					<ul className="socialMedia" style={this.state.showSocial ? {display: 'none'} : {display: 'flex'}}>
-						<li>facebook</li>
-						<li>twitter</li>
+					<li className="socialDropDown" onClick={this.showSocialDropDown}>social {this.renderDropDownGlyph()}</li>
+					<ul className="socialMedia" onMouseLeave={this.showSocialDropDown} style={this.state.showSocial  == false ? {display: 'none'} : {display: 'flex'}}>
+						<li><a href="#">facebook</a></li>
+						<li><a href="#">twitter</a></li>
 					</ul>
 				</ul>	
 			</nav>
